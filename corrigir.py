dd7a5813fef38b0b5a2bc76d3c0bd7e8863c0a66
DjonysDalmy <Djonysdalmy@gmail.com>
2022-07-30 19:09:02 -0300

    feat: Finalizado formatação
    Verifica casos errados e corrige

---
+++ corrigir.py
@@ -0,0 +1,10 @@
+from variaveis import *
+
+def corrigir(lista_respostas):
+    alunos = []
+    for resposta in lista_respostas:
+        pessoa = lista_base_alunos
+        pessoa[0] = resposta[2]
+        alunos.append(pessoa)
+    print()
+        
