e178441027fef66ee13bd54c1219682bdc884159
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-08-12 18:33:36 -0300

    feat: detalhado dos alunos feito

--- src/gerar_json_aluno.py
+++ src/gerar_json_aluno.py
@@ -1,4 +1,8 @@
+# TODO: por questão de tempo, não fiz um bom if para verificar se o aluno fez ingles ou espanhol, vou arrumar melhor depois, talvez em um simuenem?
+
+from dis import dis
 from gerar_json_disciplina import *
+from variaveis import areas_ufsc
 
 def gerar_json_alunos(correcao, dados_alunos, gabarito):
     students_dataset = []
@@ -8,10 +12,6 @@ def gerar_json_alunos(correcao, dados_alunos, gabarito):
         cpf = pegar_cpf(nome_aluno, dados_alunos)
         posicao = encontrar_posicao(correcao, nota_total)
         
-    
-        subjects = gerar_estrutura_disciplinas()
-        subjects = gerar_detalhado_disciplina_estrutura(subjects, correcao, gabarito)
-        
         students_dataset.append(
             {
                 "info": {
@@ -20,13 +20,84 @@ def gerar_json_alunos(correcao, dados_alunos, gabarito):
                     "total": nota_total,
                     "position": posicao 
                 },
-                "details": subjects 
+                "details": gerar_details_aluno(aluno, gabarito)  
                 # TODO: Incluir redação no detalhado
                 # TODO: Incluir msg do tutor
             }
         )
-
     return students_dataset
+
+def gerar_details_aluno(aluno, gabarito):
+    details = {}
+    for area_ufsc in areas_ufsc:
+        lingua_estrangeira_aluno = aluno[83]
+        for chave_dicionario, disciplinas_area in area_ufsc.items():
+            details[chave_dicionario] = {}
+            for disciplina in disciplinas_area:
+                # TODO: mudar esse if depois tá muito feito
+                if disciplina == "Inglês" or disciplina == "Espanhol":
+                    if lingua_estrangeira_aluno == disciplina[0].lower():
+                        details[chave_dicionario][disciplina] = {
+                            "question_numbers": 0,
+                            "general_percent": 0,
+                            "answered": []
+                        }
+                        continue
+                    else:
+                        continue
+
+                details[chave_dicionario][disciplina] = {
+                        "question_numbers": 0,
+                        "general_percent": 0,
+                        "answered": []
+                    }
+
+    details = gerar_anwsers_aluno(aluno, gabarito, details)
+    details = calcular_general_porcent(details, aluno)
+    return details
+
+def gerar_anwsers_aluno(aluno, gabarito, details):
+    for questao in gabarito:
+        disciplina = questao[0]
+        numero_questao = questao[2]
+        resposta_aluno = aluno[numero_questao + 1]
+        area = questao[5]
+        pontuacoes = 0.
+        lingua_estrangeira_aluno = aluno[83]
+        if "ANULADA" not in str(aluno[numero_questao + 1]):
+            pontuacoes += aluno[numero_questao + 1]
+        # TODO: mudar esse if depois tá muito feito
+        if disciplina == "Inglês" or disciplina == "Espanhol":
+                    if lingua_estrangeira_aluno == disciplina[0].lower():
+                        details[area][disciplina]["question_numbers"] += 1
+                        details[area][disciplina]["general_percent"] += pontuacoes
+                        details[area][disciplina]["answered"].append(resposta_aluno)
+                        continue
+                    else:
+                        continue
+
+        details[area][disciplina]["question_numbers"] += 1
+        details[area][disciplina]["general_percent"] += pontuacoes
+        details[area][disciplina]["answered"].append(resposta_aluno)
+    return details
+
+def calcular_general_porcent(details, aluno):
+    lingua_estrangeira_aluno = aluno[83]
+    for area_ufsc in areas_ufsc:
+        for chave_dicionario, disciplinas_area in area_ufsc.items():
+            for disciplina in disciplinas_area:
+                # TODO: mudar esse if depois tá muito feito
+                if disciplina == "Inglês" or disciplina == "Espanhol":
+                    if lingua_estrangeira_aluno == disciplina[0].lower():
+                        details[chave_dicionario][disciplina]["general_percent"] = round(details[chave_dicionario][disciplina]["general_percent"] / \
+                    details[chave_dicionario][disciplina]["question_numbers"] * 100, 2)
+                        continue
+                    else:
+                        continue
+
+                details[chave_dicionario][disciplina]["general_percent"] = round(details[chave_dicionario][disciplina]["general_percent"] / \
+                    details[chave_dicionario][disciplina]["question_numbers"] * 100, 2)
+    return details
 
 def pegar_cpf(nome_aluno, dados_alunos):
     cpf = ""
