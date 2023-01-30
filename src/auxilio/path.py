caa49f556d55b0e32118c2386ecc96e0b67f7397
Marcelo Ecco <marceloecco1@gmail.com>
2023-01-30 13:49:37 -0300

    fix: definir as variáveis coletadas pela gui no projeto principal possibilitando o uso delas no algoritimo principal.

---
+++ src/auxilio/path.py
@@ -0,0 +1,19 @@
+import os
+
+
+# Cria pasta relatorio(1) se existir cria relatorio(1) ...
+def get_caminho_de_saida(dir_selecioado):
+
+    cont = 0
+    while True:
+        path_saida = os.path.join(dir_selecioado, f"output({cont})")
+        if not os.path.exists(path_saida):
+            os.makedirs(path_saida)
+            break
+        cont += cont
+
+    if path_saida[-3:] == "(0)":
+        path_saida.strip("(0)")
+
+    return path_saida
+    
