7a577a2cc0e04c7f3cf0e7eef7dcdeda68d98d3c
Marcelo Ecco <marceloecco1@gmail.com>
2023-02-02 11:26:41 -0300

    style: renomear avriaveis em path.py

--- src/auxilio/path.py
+++ src/auxilio/path.py
@@ -6,20 +6,20 @@ ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 
 # adiciona relatorio ao caminho, se ja existir adiciona relatorio_1 ...
-def get_caminho_de_saida(dir_selecioado):
+def get_caminho_de_saida(dir_selecionado):
 
     cont = 0
     while True:
-        path_saida = os.path.join(dir_selecioado, f"output_{cont}")
+        path_output_arquivos_correcao = os.path.join(dir_selecionado, f"output_{cont}")
 
-        if path_saida[-2:] == "_0":
-            path_saida = path_saida.strip("_0")
+        if path_output_arquivos_correcao[-2:] == "_0":
+            path_output_arquivos_correcao = path_output_arquivos_correcao.strip("_0")
 
-        if not os.path.exists(path_saida):
+        if not os.path.exists(path_output_arquivos_correcao):
             break
         cont += 1
 
-    return path_saida
+    return path_output_arquivos_correcao
 
 
 def join_paths(*args):
